2ebe5cba28f7dda7cea1dd622b732b13ca0d920d
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-07-24 19:01:44 -0500

    add vuejs and custom element

--- block.php
+++ block.php
@@ -2,6 +2,13 @@
 
 add_action( 'enqueue_block_editor_assets', 'marcus_hiles_vuejs_gutenburg_block' );
 function marcus_hiles_vuejs_gutenburg_block() {
+ 
+    wp_register_script( 'vue', 'https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js' );
+	wp_register_script(
+		'vue-custom-element',
+		'https://unpkg.com/vue-custom-element@1.4.0/dist/vue-custom-element.js',
+		array( 'vue' )
+	);
     
     $dir = dirname( __FILE__ );
     $block_js = '/block.js';
